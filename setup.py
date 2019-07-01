c7aff08035f014c4c86f569bf0b8265139591863
Sorawit Suriyakarn <swit@bandprotocol.com>
2019-07-01 11:52:03 +0700

    Add requirements to pip dependency

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
-#!/usr/bin/env python
+import os
 from setuptools import setup
+
+
+REQUIREMENTS = [
+    line.strip()
+    for line in open(os.path.join(os.path.dirname(__file__), "requirements.txt")).readlines()
+]
 
 
 setup(
     name="pylibra",
-    version="0.0.1",
+    version="0.0.3",
+    license="MIT",
     author="Band Protocol",
     author_email="opensource@bandprotocol.com",
     description="A Python client for Libra network",
     url="https://github.com/bandprotocol/pylibra",
     packages=["pylibra"],
-    zip_safe=False,
-    install_requires=[],
+    keywords=["libra", "client", "cryptocurrency", "blockchain"],
+    install_requires=REQUIREMENTS,
     classifiers=[
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
         "Operating System :: Microsoft :: Windows",
